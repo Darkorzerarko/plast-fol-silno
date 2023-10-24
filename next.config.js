0cbb25774017300d8651a45f1867ab200b186b3a
Darkorzerarko <rz.dariusz@gmail.com>
2023-10-24 20:53:11 +0200

    Small change in next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,13 @@ const nextConfig = {
     // JAK USUNĘ/ZMIENIĘ BASE PATH TO MUSZĘ TEŻ ZMIENIĆ W STYLACH URL'E
     basePath: '/plast-fol-silno',
 
-    images: { unoptimized: true }
+    images: { 
+        unoptimized: true,
+    },
+
+    // sassOptions: {
+    //     includePaths: [path.join(__dirname, 'styles')],
+    //   }
 }
 
 module.exports = nextConfig
