3b644ba4ca66619f30927470b65adcfbe2613821
Darkorzerarko <rz.dariusz@gmail.com>
2023-11-20 21:06:47 +0100

    Github action node version changed v2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    
     // JAK USUNĘ/ZMIENIĘ BASE PATH TO MUSZĘ TEŻ ZMIENIĆ W STYLACH URL'E
     basePath: '/plast-fol-silno',
 
